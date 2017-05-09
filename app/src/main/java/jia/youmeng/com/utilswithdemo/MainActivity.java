ed3516134d0ee87af2c88af5b438687f5830f9ef
jiawexing <jiawx0768@163.com>
2017-05-09 17:16:58 +0800

    第一次提交

--- app/src/main/java/jia/youmeng/com/utilswithdemo/MainActivity.java
+++ app/src/main/java/jia/youmeng/com/utilswithdemo/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +41,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        TextView  textView=new TextView(this);
+
     }
 
     @Override
